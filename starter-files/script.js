370d6cd3d7256277a096807b6fb54d40b5f9990b
esdrasmdz <esdrasm.diazz@gmail.com>
2024-06-20 11:40:46 -0600

    Primer intervencion al ejercicio

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,5 +1,43 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
+const btnGetAnswer = document.getElementById("button");
+const ballAnswer = document.getElementById("answer");
+/*
+fetch('https://yesno.wtf/api')
+.then(function() {
+
+})
+.catch(function() {
+
+});
+
+
+fetch('https://yesno.wtf/api')
+    // Exito
+    .then(response => response.json())  // convertir a json
+    .then(json => console.log(json))    //imprimir los datos en la consola
+    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
+*/
+
+    fetch('https://yesno.wtf/api', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ mail: 'pp@pp.com', password: '123' })
+    })
+    .then(res => res.json())
+    .then(res=> {
+          console.log(res);
+    });
+
+
+
+    btnGetAnswer.addEventListener("click", fetchAnswer);
+function fetchAnswer(response){
+    response= res
+}//function
+
 
 /**
  * STEPS:
